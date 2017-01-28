1ad532e1f8047f87c2654fd458bf6233616eadb9
Ryo Yasuda <e165729@ie.u-ryukyu.ac.jp>
2017-01-28 19:36:28 +0900

    Image is complete...?

--- src/main/java/jp/ac/uryukyu/ie/e165729/act/MainPanel.java
+++ src/main/java/jp/ac/uryukyu/ie/e165729/act/MainPanel.java
@@ -4,7 +4,8 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
 
 /**
@@ -17,6 +18,36 @@ public class MainPanel extends JPanel {
 
     private Image heroImage;
 
+    // 行と列の大きさ(ピクセル)
+    private static final int row = 15;
+    private static final int col = 15;
+
+    // チップセットのサイズ(ピクセル)
+    private static final int cs = 32;
+
+    // マップ: 0=床，1=壁
+    private int [][] map = {
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,1,1,1,1,1,0,0,0,0,1},
+            {1,0,0,0,0,1,0,0,0,1,0,0,0,0,1},
+            {1,0,0,0,0,1,0,0,0,1,0,0,0,0,1},
+            {1,0,0,0,0,1,0,0,0,1,0,0,0,0,1},
+            {1,0,0,0,0,1,1,0,1,1,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}};
+
+    //　チップセット
+    private Image floorImage;
+    private Image wallImage;
+
+
     public MainPanel(){
         // パネルの推奨サイズの設定
         setPreferredSize(new Dimension(width,height));
@@ -28,13 +59,32 @@ public class MainPanel extends JPanel {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        // 勇者のイメージを入力した座標に表示
-        g.drawImage(heroImage, 0, 0, this);
+        // マップを描く
+        drawMap(g);
     }
 
-    private void loadImage(){
-        ImageIcon icon = new ImageIcon("/Users/e165729/IdeaProjects/Report7/src/main/java/jp/ac/uryukyu/ie/e165729/image/Hero.png");
-        heroImage = icon.getImage();
+    private  void loadImage(){
+        ImageIcon icon = new ImageIcon("/Users/e165729/IdeaProjects/Report7/src/main/java/jp/ac/uryukyu/ie/e165729/image/floor.gif");
+        floorImage = icon.getImage();
+
+        icon = new ImageIcon("/Users/e165729/IdeaProjects/Report7/src/main/java/jp/ac/uryukyu/ie/e165729/image/wall.gif");
+        wallImage = icon.getImage();
     }
 
+    private void drawMap(Graphics g){
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                // mapの値に応じて絵を描く
+                switch (map[i][j]){
+                    case 0: // 床
+                        g.drawImage(floorImage, j*cs, i*cs, this);
+                        break;
+
+                    case 1: // 壁
+                        g.drawImage(wallImage, j*cs, i*cs, this);
+                        break;
+                }
+            }
+        }
+    }
 }
